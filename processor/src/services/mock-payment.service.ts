7ecf07d83723c37c0ca483553a57a4f2f6c3d69b
abirajs <meisterheidi89@gmail.com>
2025-06-23 15:24:39 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -258,10 +258,10 @@ console.log('status-handler');
 
 
   public async get_customer_addrs() {
-    const CartDetails = await this.ctCartService.getCart({
-      id: getCartIdFromContext(),
-    });
-    return JSON.stringify(CartDetails);
+    // const CartDetails = await this.ctCartService.getCart({
+    //   id: getCartIdFromContext(),
+    // });
+    return 'checking';
   }
   /**
    * Create payment
