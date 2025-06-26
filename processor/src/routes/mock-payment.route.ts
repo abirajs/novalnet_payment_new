7b662f2bbb846861194b2e9878ddbe94d5253209
abirajs <meisterheidi89@gmail.com>
2025-06-26 17:41:52 +0530

    Update mock-payment.route.ts

--- processor/src/routes/mock-payment.route.ts
+++ processor/src/routes/mock-payment.route.ts
@@ -41,7 +41,7 @@ fastify.post('/test', async (request, reply) => {
       },
       custom: {
         input1: 'accesskey',
-        inputval1: cartt,
+        inputval1: 'cartt',
       },
     };
 
